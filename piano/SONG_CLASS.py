7748dc1ea72d98b81446dad1170dc4e888f35149
aFF0gato <87004940+atortellini@users.noreply.github.com>
2024-03-26 14:05:28 -0700

    Removal of AUTOPLAYER code

--- piano/SONG_CLASS.py
+++ piano/SONG_CLASS.py
@@ -32,20 +32,6 @@ class Song:
 		self.num_notes = len(self.notes)
 
 
-	# def set_playback_method(self):
-	# 	req_method = -1
-	# 	while ((req_method != AUTOPLAY) and (req_method != GUIDED_PLAY)):
-	# 		req_method = int(input("Enter requested playback method:\n 0. AUTOPLAY\n 1. GUIDED_PLAY"))
-	# 	self.playback_method = req_method
-	# 	if req_method == AUTOPLAY:
-	# 		tempo = 0
-	# 		while ((tempo < 30) or (tempo > 400)):
-	# 			tempo = int(input("Tempo in BMP: (30-400):"))
-	# 		self.bpm = tempo
-
-
-
-
 
 
 
