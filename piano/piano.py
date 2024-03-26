e40a8353d57fa7fed2d935ffae4bcdbdb7d239b1
aFF0gato <87004940+atortellini@users.noreply.github.com>
2024-03-26 14:05:01 -0700

    Removal of AUTOPLAYER code

--- piano/piano.py
+++ piano/piano.py
@@ -15,15 +15,6 @@ currently_pressed_notes = set()
 SONG_COMPLETED = threading.Event()
 
 
-
-#usr_song.set_playback_method()
-
-
-
-# def on_press_alt(key):
-# 	global key_pressed_alt
-# 	key_pressed_alt = True
-
 def on_press(key):
 	if key == keyboard.KeyCode.from_char('='):
 		event_queue.put(PRESS)
@@ -65,32 +56,13 @@ if __name__ == "__main__":
 	usr_song = SONG_CLASS.Song()
 	usr_song.add_notes()
 	print("Notes ready to play, press '=' whenever...")
-	# if usr_song.playback_method == SONG_CLASS.GUIDED_PLAY: 
 	listener = keyboard.Listener(on_press=on_press, on_release=on_release)
 	listener.start()
 	event_thread = threading.Thread(target=process_events)
 	event_thread.start()
 
 	SONG_COMPLETED.wait()	
-	# else:
-	# 	global key_pressed_alt
-	# 	key_pressed_alt = False
-	# 	current_note = 0
-	# 	tempo = 60 / usr_song.bpm
-	# 	listener = keyboard.Listener(on_press=on_press_alt)
-	# 	listener.start()
-	# 	print("Press any key to begin")
-	# 	while not(key_pressed_alt):
-	# 		time.sleep(1)
-	# 	for notes in usr_song.notes:
-	# 		for note in notes:
-	# 			maKeys.pressLetter(note)
-	# 			currently_pressed_notes.add(note)
-	# 		for note in currently_pressed_notes:
-	# 			maKeys.releaseLetter(note)
-	# 		currently_pressed_notes.clear()
-	# 		time.sleep(tempo)
-
+	
 	listener.stop()
 	print("Song completed.\n")
 
